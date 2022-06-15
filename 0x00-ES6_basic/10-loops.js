3dd0b8fd8518c33f4b2496c416e3627c410ea708
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-15 21:53:13 +0300

    eslint fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-  
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-  
-    return newArray;
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
 }
