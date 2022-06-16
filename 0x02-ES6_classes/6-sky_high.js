91d210f691c6720276976b25bb47e2ffc386aa52
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-17 00:59:02 +0300

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+/*
+Import Building from 5-building.js.
+
+Implement a class named SkyHighBuilding that extends from Building:
+
+Constructor attributes:
+sqft (Number) (must be assigned to the parent class Building)
+floors (Number)
+Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+Implement a getter for each attribute.
+Override the method named evacuationWarningMessage and,
+return the following string Evacuate slowly the NUMBER_OF_FLOORS floors.
+*/
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
