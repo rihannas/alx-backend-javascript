72234bd2fdbdeae252f33a809dcbaf9d25675d1e
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-17 00:34:40 +0300

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+/*
+Import the class Currency from 3-currency.js
+
+Implement a class named Pricing:
+
+Constructor attributes:
+amount (Number)
+currency (Currency)
+Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+Implement a getter and setter for each attribute.
+Implement a method named displayFullPrice that returns the attributes in the following-
+format amount currency_name (currency_code).
+Implement a static method named convertPrice.
+It should accept two arguments: amount (Number), conversionRate (Number).
+The function should return the amount multiplied by the conversion rate.
+*/
+
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
