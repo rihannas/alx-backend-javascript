a413bad19be0148b8ead8ce9bfe23f6f095df946
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-16 22:25:39 +0300

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/* Implement a class named ClassRoom:
+Prototype: export default class ClassRoom
+It should accept one attribute named maxStudentsSize (Number)
+and assigned to _maxStudentsSize
+*/
+
+// a class
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
