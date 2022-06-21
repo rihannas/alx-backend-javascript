8aedc4e1ec21d27880bba1a332c60188e58921f8
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-22 00:24:53 +0300

    update;

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const arrayStudents = new Array([
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }]);
-
-  return (arrayStudents);
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
