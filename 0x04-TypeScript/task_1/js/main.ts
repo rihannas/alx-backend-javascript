48dbf3b079083a7fe3f7be68ecba9d3495ed9d39
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 00:08:13 +0300

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,16 @@
+// task 1
+
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [key: string]: any
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+//task 2
+
+interface Directors extends Teacher {
+    numberOfReports: number;
 }
