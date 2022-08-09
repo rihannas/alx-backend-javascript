258157a1e6430817af0662331284e492c388a2f5
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-10 01:45:53 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+/* create a function named displayMessage that prints in STDOUT the string argument. */
+
+const displayMessage = (message) => {
+    console.log(message)
+};
+
+module.exports = displayMessage;
