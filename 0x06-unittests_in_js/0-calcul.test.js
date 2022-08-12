2da0a7b595ebcd046d90dde1d68e2b1687600828
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-12 04:52:19 +0300

    update file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,22 @@
 const assert = require('assert');
-const {calculateNumber} = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('round the first argument', () => {
-    assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(1.8, 0), 2);
+  it('checks the output', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
-
-  it('round the second argument', () => {
-    assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.8), 2);
+  it('negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
   });
-
-  it('should return the right number', () => {
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(0, 1.2), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
+  it('checks arguments', () => {
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
