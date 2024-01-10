a34ea6c9f500f6df103716b6a9fb3f57d855adea
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-11 01:57:45 +0300

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+const getFullResponseFromAPI = (sucess) => {
+  return new Promise((resolve, reject) => {
+    if (sucess) {
+      resolve({ status: 200, body: 'sucess' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+};
+export default getFullResponseFromAPI;
