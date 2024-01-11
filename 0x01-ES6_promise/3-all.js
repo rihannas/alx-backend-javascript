c3473c8267f3e93aa04e36459d32c72464996a53
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-11 05:07:24 +0300

    fix mistake

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,21 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+function handleProfileSignup() {
   // Use Promise.all to collectively resolve promises
-  Promise.all([uploadPhoto(), createUser()]).then(
-    ([photoResult, userResult]) => {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResult, userResult]) => {
       // Extract information from resolved promises
       const { body: photoBody } = photoResult;
       const { firstName, lastName } = userResult;
 
       // Log the extracted information to the console
       console.log(photoBody, firstName, lastName);
-    }
-  );
+    })
+    .catch(() => {
+      // Log an error message if there is an issue
+      console.log('Signup system offline');
+    });
 }
+
+// Call the function to initiate the process
+handleProfileSignup();
