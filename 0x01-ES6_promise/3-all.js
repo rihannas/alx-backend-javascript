c988bbea3064e1fe1a5d976cfe7c53bae739cb53
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-11 04:59:13 +0300

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  // Use Promise.all to collectively resolve promises
+  Promise.all([uploadPhoto(), createUser()]).then(
+    ([photoResult, userResult]) => {
+      // Extract information from resolved promises
+      const { body: photoBody } = photoResult;
+      const { firstName, lastName } = userResult;
+
+      // Log the extracted information to the console
+      console.log(photoBody, firstName, lastName);
+    }
+  );
+}
