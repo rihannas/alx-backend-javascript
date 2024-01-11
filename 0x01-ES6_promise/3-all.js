842e0173fe6bd1c318b91e3ca1fc4e91dd37a3f6
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-12 02:23:58 +0300

    export func

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   // Use Promise.all to collectively resolve promises
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
