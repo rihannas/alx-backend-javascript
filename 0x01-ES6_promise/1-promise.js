b7991835a549a0774ae43ca876653f75becb502b
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-11 03:24:22 +0300

    task 1 captilise s

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
-function getFullResponseFromAPI(sucess) {
+export default function getFullResponseFromAPI(sucess) {
   return new Promise((resolve, reject) => {
     if (sucess) {
-      resolve({ status: 200, body: 'sucess' });
+      resolve({ status: 200, body: 'Sucess' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
   });
 }
-
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
