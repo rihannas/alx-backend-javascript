f6ee4d8f3a77493fc9e63e5cf491e88acc85bc9b
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-11 03:25:45 +0300

    task 1 fix spelling

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@
 export default function getFullResponseFromAPI(sucess) {
   return new Promise((resolve, reject) => {
     if (sucess) {
-      resolve({ status: 200, body: 'Sucess' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
