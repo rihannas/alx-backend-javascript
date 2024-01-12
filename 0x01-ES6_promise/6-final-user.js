49b4322f3f783aa449451348ce6192fdefd1442e
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-12 03:36:16 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 // Exporting the handleProfileSignup function
-export function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   // Call the signUpUser and uploadPhoto functions with the provided arguments
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
