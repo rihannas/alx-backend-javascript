edf1eccaf8f8988398523736c87e417821cc9fba
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-14 00:42:51 +0300

    task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,23 @@
+import divideFunction from './8-try.js';
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(...[value, 'Guardrail was processed']);
+  } catch (e) {
+    queue.push(...['Error: ' + e.message, 'Guardrail was processed']);
+  }
+
+  return queue;
+}
+
+console.log(
+  guardrail(() => {
+    return divideFunction(10, 2);
+  })
+);
+console.log(
+  guardrail(() => {
+    return divideFunction(10, 0);
+  })
+);
